a5b5dd0e1680ae85219e8eb725875219cd774217
Brian <brian.mcclr@gmail.com>
2018-03-18 12:25:02 +0100

    view set instance

--- src/modules/app/views/ExampleViewSetInstance.vue
+++ src/modules/app/views/ExampleViewSetInstance.vue
@@ -1,71 +1,23 @@
-<template>
-  <div>
-    <div v-if="errors" v-for="error in errors">
-      {{error}}
-    </div>
-    <div >
-      <retrieve-example-view-set-component v-if="!edit" />
-      <update-example-serializer v-else />
-      <a href="#" v-if="edit"  @click.prevent.stop="edit = false">Cancel</a>
-      <a href="#" v-else @click.prevent.stop="edit = true">Edit</a>
-
-      <destroy-example-view-set-component/>
-
-    </div>
-  </div>
-</template>
-
 <script>
-  import { mapState, mapActions } from 'vuex'
+  import Vue from 'vue'
 
+  import ViewSetInstance from '../../../views/ViewSetInstance'
   import DestroyExampleViewSetComponent from '.././components/DestroyExampleViewSetComponent.vue'
   import UpdateExampleSerializer from '.././components/UpdateExampleSerializer.vue'
   import RetrieveExampleViewSetComponent from '.././components/RetrieveExampleViewSetComponent.vue'
 
-  export default {
+  export default Vue.extend({
+    mixins: [ViewSetInstance],
     data () {
       return {
-        edit: false,
+        namespace: 'app/Example',
+        routeName: 'example-detail'
       }
     },
-    computed: {
-      ...mapState('app/Example', {
-        object: function (state) {
-          return state.objects.all[this.$route.params.pk]
-        },
-        errors: state => state.errors
-      }),
-      routeDescription () {
-        return {
-          name: 'example-detail',
-          params: {
-            pk: this.object.id.value,
-          },
-        }
-      },
-    },
-    watch: {
-      '$route' (to, from) {
-        this.edit = false
-        this.retrieve(to)
-      },
-    },
-    created () {
-      this.retrieve(this.$route)
-    },
-    methods: {
-      ...mapActions('app/Example', [
-        'retrieve',
-      ]),
-    },
     components: {
-      UpdateExampleSerializer,
-      RetrieveExampleViewSetComponent,
-      DestroyExampleViewSetComponent,
+      UpdateComponent: UpdateExampleSerializer,
+      RetrieveComponent: RetrieveExampleViewSetComponent,
+      DestroyComponent: DestroyExampleViewSetComponent,
     },
-  }
-
+  })
 </script>
-
-<style>
-</style>
--- src/views/ViewSetInstance.vue
+++ src/views/ViewSetInstance.vue
@@ -1,13 +1,65 @@
 <template>
-  
+  <div>
+    <div v-if="errors" v-for="error in errors">
+      {{error}}
+    </div>
+    <div>
+      <retrieve-component v-if="!edit"/>
+      <update-component v-else/>
+      <a href="#" v-if="edit" @click.prevent.stop="edit = false">Cancel</a>
+      <a href="#" v-else @click.prevent.stop="edit = true">Edit</a>
+      <destroy-component/>
+    </div>
+  </div>
 </template>
 
 <script>
+  import { mapState, mapActions } from 'vuex'
+  import stateModule from '../util'
+
   export default {
-    name: 'view-set-instance',
+    data () {
+      return {
+        edit: false,
+      }
+    },
+    computed: {
+      ...mapState({
+        stateModule,
+        object (state) {
+          return this.stateModule.objects.all[this.$route.params.pk]
+        },
+        errors (state) {
+          return this.stateModule.errors
+        },
+      }),
+      routeDescription () {
+        return {
+          name: this.routeName,
+          params: {
+            pk: this.object.id.value,
+          },
+        }
+      },
+    },
+    watch: {
+      $route (to, from) {
+        this.edit = false
+        this.retrieve(to)
+      },
+    },
+    created () {
+      this.retrieve(this.$route)
+    },
+    methods: {
+      ...mapActions({
+        retrieve (dispatch, route) {
+          return dispatch(`${this.namespace}/retrieve`, route)
+        }
+      }),
+    },
   }
 </script>
 
-<style scoped>
-
+<style>
 </style>
