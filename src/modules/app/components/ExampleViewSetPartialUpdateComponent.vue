e9a1d27bf326340c6a4eb94a65424b9871734cb2
Brian <brian.mcclr@gmail.com>
2018-03-24 13:30:07 +0100

    Added partial update component

---
+++ src/modules/app/components/ExampleViewSetPartialUpdateComponent.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue'
+import UpdateViewSetComponent from '../../../components/UpdateViewSetComponent'
+import ExampleSerializer from './ExampleSerializer'
+
+export default Vue.extend({
+  mixins: [UpdateViewSetComponent],
+  components: { InstanceForm: ExampleSerializer },
+  data() {
+    return {
+      namespace: 'app/Example',
+      objectName: 'Example',
+      routeName: 'example-detail',
+    }
+  },
+})
+</script>
