42accc508cd0f6ce1b9e54f3132d5a34665dc600
Brian <brian.mcclr@gmail.com>
2018-03-24 12:31:33 +0100

    modified list to omatch django changes

--- src/modules/app/components/ExampleViewSetListComponent.vue
+++ src/modules/app/components/ExampleViewSetListComponent.vue
@@ -1,15 +1,15 @@
 <script>
-  import Vue from 'vue'
-  import ListViewSetComponent from '../../../components/ListViewSetComponent'
+import Vue from 'vue'
+import ListViewSetComponent from '../../../components/ListViewSetComponent'
 
-  export default Vue.extend({
-    mixins: [ListViewSetComponent],
-    data () {
-      return {
-        namespace: 'app/Example',
-        routeName: 'example-list',
-        detailRouteName: 'example-detail',
-      }
-    },
-  })
+export default Vue.extend({
+  mixins: [ListViewSetComponent],
+  data() {
+    return {
+      namespace: 'app/Example',
+      routeName: 'example-list',
+      detailRouteName: 'example-detail',
+    }
+  },
+})
 </script>
