4c2b68fd08073c002be71260b0f0daf765b65359
Brian <brian.mcclr@gmail.com>
2018-03-24 13:36:49 +0100

    modified destroy formatting

--- src/modules/app/components/ExampleViewSetDestroyComponent.vue
+++ src/modules/app/components/ExampleViewSetDestroyComponent.vue
@@ -1,23 +1,23 @@
 <script>
-  import Vue from 'vue'
-  import { mapState } from 'vuex'
+import Vue from 'vue'
+import { mapState } from 'vuex'
 
-  import DestroyViewSetComponent from '../../../components/DestroyViewSetComponent'
+import DestroyViewSetComponent from '../../../components/DestroyViewSetComponent'
 
-  export default Vue.extend({
-    mixins: [DestroyViewSetComponent],
-    data () {
-      return {
-        namespace: 'app/Example',
-        routeName: 'example-detail',
+export default Vue.extend({
+  mixins: [DestroyViewSetComponent],
+  data() {
+    return {
+      namespace: 'app/Example',
+      routeName: 'example-detail',
+    }
+  },
+  computed: {
+    ...mapState('app/Example', {
+      object: function(state) {
+        return state.objects.local[this.$route.params.pk]
       }
-    },
-    computed: {
-      ...mapState('app/Example', {
-        object: function (state) {
-          return state.objects.local[this.$route.params.pk]
-        }
-      }),
-    },
-  })
+    }),
+  },
+})
 </script>
